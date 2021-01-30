3ea68574cfa17d9eb3d94f7ebaa2a07b657cea8b
mwiby <markuswb77@gmail.com>
2021-01-30 14:06:56 +0100

    Footer svg and structure implemented

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,33 +1,18 @@
 import styles from "./Footer.module.css";
+import { Container } from "../";
 
 export default function Footer() {
   return (
     <div className={styles.container}>
-      <svg
-        width="1263"
-        height="724"
-        viewBox="0 0 19063 5724"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M1177.32 429.49C143.596 429.49 0 222.811 0 222.811V2917V5724H9521.58H19063V1411.5C19063 -984.302 16549.6 590.268 15315.9 92.8451C14082.2 -404.577 11965.1 1288.04 10326.7 674.496C8688.21 60.9521 8919.67 1236.42 5958.51 429.49C2997.36 -377.443 2211.05 429.489 1177.32 429.49Z"
-          fill="url(#paint0_linear)"
-        />
-        <defs>
-          <linearGradient
-            id="paint0_linear"
-            x1="9502.95"
-            y1="-784.508"
-            x2="12077.8"
-            y2="8168.36"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#141E30" />
-            <stop offset="0.59375" stopColor="#004863" stopOpacity="0.9" />
-          </linearGradient>
-        </defs>
-      </svg>
+      <Container>
+        <div style={styles.logo}></div>
+        <div className={styles.grid}>
+          <div>Test1</div>
+          <div>Test2</div>
+          <div>Test3</div>
+          <div>Test4</div>
+        </div>
+      </Container>
     </div>
   );
 }
