37c597e951ff47054493d2a594a9e34cb0fc530b
mwiby <markuswb77@gmail.com>
2021-02-03 05:17:52 +0100

    Designed Hero section

--- sections/Hero/Hero.js
+++ sections/Hero/Hero.js
@@ -1,13 +1,29 @@
+import Image from "next/image";
 import styles from "./Hero.module.css";
 
 export default function Hero() {
   return (
-    <div className={styles.container}>
-      <h1>Player FLACKO</h1>
-      <p>I make Call of Duty Warzone content .</p>
-      <button className={styles.buttonSecondary} type="button">
-        Contact me
-      </button>
-    </div>
+    <section className={styles.container}>
+      <div className={styles.img}>
+        <Image
+          src="/images/logo1.png"
+          alt="Player logo"
+          className={styles.image}
+          height={150}
+          width={150}
+        />
+      </div>
+      <div>
+        <h1>Player FLACKO</h1>
+        <p>I make Call of Duty Warzone content .</p>
+        <a
+          href="mailto:markusb@nordevo.no"
+          target="_blank"
+          className={styles.btn}
+        >
+          Contact
+        </a>
+      </div>
+    </section>
   );
 }
